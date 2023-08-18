b321cf6ae8f3311bcf4c16aba3514052e4cb0520
Santiago <esematillajr@gmail.com>
2023-08-18 15:15:49 +0200

    Iteration 2 finished

--- src/main/java/rpgCombat/Character.java
+++ src/main/java/rpgCombat/Character.java
@@ -5,39 +5,81 @@ public class Character {
     int level;
     boolean alive;
     int strength;
-    int healing;
-    public Character(int health, int level, boolean alive, int strength, int healing) {
+
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+
+    public Character(int health, int level, boolean alive, int strength) {
         this.health = health;
         this.level = level;
         this.alive = alive;
         this.strength = strength;
-        this.healing = healing;
     }
 
-    public String receiveDamage(int damage){
-         if(damage>this.health){
-             this.health = 0;
-            return "This character is DEAD";
-        }else{
-             return "The character has recieved " + damage + " points of DAMAGE. Current HEALTH = " +this.health+ " points";
+    public String receiveDamage(double damage, Character Pepe){
+         if(this!=Pepe){
+             if(this.getHealth()<=0){
+                 this.health = 0;
+                 this.setAlive(false);
+                 return "This character is DEAD";
+             }else{
+                 if(this.level - Pepe.level>=5){
+                    damage=damage*0.5;
+                    this.health-=damage;
+                    return "Daño aplicado, reducido al 50% The character has recieved " + damage + " points of DAMAGE. Current HEALTH = " +this.health+ " points";
+
+                 }else if(this.level - Pepe.level<=5){
+                     damage=damage*1.5;
+                     this.health-=damage;
+                     return "Daño aplicado, reducido al 50% The character has recieved " + damage + " points of DAMAGE. Current HEALTH = " +this.health+ " points";
+
+                 }else{
+                     this.health-=damage;
+                     return "The character has recieved " + damage + " points of DAMAGE. Current HEALTH = " +this.health+ " points";
+                 }
+             }
+         }else{
+             return "This character cannot attack himself";
          }
     }
-    /*public String healCompanion(){
-        if(health>0){
-            health += healing;
-            return "The character has been healed +"+healing+" health points";
-        }else if(health){
-
-        }
-        if(this.alive == false){
-            return "You can´t HEAL a DEAD Character";
-        }else{
-            while(this.health<1000){
-                health += healing;
-                return "The character has been healed +"+healing+" health points";
+    public String heal(Character Pepe){
+        if(this==Pepe){
+            if(alive){
+                this.setHealth(1000);
+                return "This Character has been healed.";
+            }else{
+                return "This Character is dead and cannot be healed";
             }
+        }else{
+            return "This Character cannot heal another character.";
         }
-    }*/
+    }
 
 
 
--- src/test/java/rpgCombat/CharacterTest.java
+++ src/test/java/rpgCombat/CharacterTest.java
@@ -7,41 +7,69 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class CharacterTest {
-    int health = 1000;
-    int level = 1;
-    boolean alive = true;
-    int strength = 100;
 
-    int healing = 150;
-
-    Character character1 = new Character(health, level, alive, strength, healing);
+    Character character1 = new Character(1000, 1, true, 100);
+    Character character2 = new Character(800, 1, true, 200);
 
     @Test
     public void health_should_start_at_1000(){
-        assertTrue(character1.health<=1000);
+        assertTrue(character1.getHealth()<=1000);
     }
 
     @Test
     public void level_should_start_at_1(){
-        assertTrue(character1.level==1);
+
+        assertTrue(character1.getLevel()==1);
     }
 
     @Test
     public void alive_should_be_true(){
+
         assertTrue(character1.alive);
     }
 
     @Test
     public void strength_should_be_100(){
-        assertTrue(character1.strength==100);
+
+        assertTrue(character1.getStrength()==100);
     }
     @Test
     public void damage_should_substract_from_health(){
-        assertEquals("The character has recieved " + 50 + " points of DAMAGE. Current HEALTH = " +this.health+ " points",character1.receiveDamage(50));
+        character1.receiveDamage(50, character2);
+        assertEquals(950, character1.getHealth());
     }
     @Test
-    public void healing_should_be_150(){
-        assertTrue(character1.healing==150);
+    public void a_Character_can_Heal_a_Character(){
+        character1.setHealth(900);
+        character1.heal(character1);
+        assertEquals(1000, character1.getHealth());
     }
 
+    //iteration 2
+
+    @Test
+    public void A_Character_cannot_Deal_Damage_to_itself(){
+        character1.receiveDamage(100, character1);
+        assertEquals(1000, character1.getHealth());
+    }
+    @Test
+    public void A_Character_can_only_heal_to_itself(){
+        character1.heal(character1);
+        assertEquals(1000, character1.getHealth());
+    }
+    @Test
+    public void If_the_target_is_5_or_more_Levels_above_the_attacker_Damage_is_reduced_by_50_percent(){
+        character1.setLevel(7);
+        character2.setLevel(1);
+        character1.receiveDamage(200, character2);
+        assertEquals(900, character1.getHealth());
+    }
+
+    @Test
+    public void If_the_target_is_5_or_more_Levels_below_the_attacker_Damage_is_increased_by_50_percent(){
+        character1.setLevel(1);
+        character2.setLevel(7);
+        character1.receiveDamage(200, character2);
+        assertEquals(700, character1.getHealth());
+    }
 }
